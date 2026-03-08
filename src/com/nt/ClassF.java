376ab9d3418923318c60cb5321f7aaf4338f1115
jhanu <jhansidasari0811@gmail.com>
2026-03-08 10:43:33 +0530

    "new Class is added"

---
+++ src/com/nt/ClassF.java
@@ -0,0 +1,18 @@
+package com.nt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ClassF {
+
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Hi new one added");
+
+	}
+}
